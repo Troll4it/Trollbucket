b6f4318a3726580bdeb616137230c3763e3bcca4
tangpeng <www.troll4it@163.com>
2022-04-06 09:08:44 +0800

    Leetcode 移除元素

--- modules/rich/src/main/java/com/troll/rich/Rich.kt
+++ modules/rich/src/main/java/com/troll/rich/Rich.kt
@@ -1,1 +1,1 @@
-package com.troll.rich/** * author : TangPeng * date : 3/19/22 21:48 * description :  提供计算 */class Rich {    companion object {        @JvmStatic        fun main(args: Array<String>) {            rich(1.46, 10000.0, 1.1)        }    }}
+package com.troll.rich/** * author : TangPeng * date : 3/19/22 21:48 * description :  提供计算 */class Rich {    companion object {        @JvmStatic        fun main(args: Array<String>) {            rich(1.07, 5000.0, 1.05)        }    }}
