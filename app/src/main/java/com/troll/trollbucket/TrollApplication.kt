9dabbe919a9c1de97bfe350ece1a1589e43a39de
tangpeng <www.troll4it@163.com>
2023-01-09 18:19:09 +0800

    创建全项目的common组件

--- app/src/main/java/com/troll/trollbucket/TrollApplication.kt
+++ app/src/main/java/com/troll/trollbucket/TrollApplication.kt
@@ -1,1 +1,1 @@
-package com.troll.trollbucketimport com.troll.common.CommonApplication/** * author : TangPeng * date : 3/7/22 16:16 * description : */class TrollApplication : CommonApplication() {}
+package com.troll.trollbucketimport com.troll.common.CommonApplication/** * author : TangPeng * date : 3/7/22 16:16 * description : */class TrollApplication : CommonApplication() {}
---
+++ wanAndroid/main/src/main/java/wan/main/MainApplication.kt
@@ -0,0 +1,1 @@
+package wan.mainimport com.troll.common.CommonApplication/** * author : TangPeng * date : 1/4/23 16:58 * description : */class MainApplication : CommonApplication()
