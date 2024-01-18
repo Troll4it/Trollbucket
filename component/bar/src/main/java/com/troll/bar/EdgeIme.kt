ce4e30fbeab96ff8040a5ce9abe2c494b4a59b0b
tangpeng <tangpeng@xmiles.cn>
2024-01-18 15:04:33 +0800

    适配

---
+++ component/bar/src/main/java/com/troll/bar/EdgeIme.kt
@@ -0,0 +1,24 @@
+package com.troll.bar
+
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import cn.jailedbird.edgeutils.RootViewDeferringInsetsCallback
+
+object EdgeIme {
+    /**
+     * TODO 1.apply margin and padding
+     *      2.save origin padding or margin
+     */
+    fun applyImeAnimation(view: View) {
+        val deferringInsetsListener = RootViewDeferringInsetsCallback(
+            persistentInsetTypes = WindowInsetsCompat.Type.systemBars(),
+            deferredInsetTypes = WindowInsetsCompat.Type.ime()
+        )
+        // RootViewDeferringInsetsCallback is both an WindowInsetsAnimation.Callback and an
+        // OnApplyWindowInsetsListener, so needs to be set as so.
+        ViewCompat.setWindowInsetsAnimationCallback(view, deferringInsetsListener)
+        ViewCompat.setOnApplyWindowInsetsListener(view, deferringInsetsListener)
+
+    }
+}
